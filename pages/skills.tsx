5b9dcfe4c4ba381088b13cd306e05f260681b44f
Shawn DI <ShawnsuDI@Shawns-MBP13-2018.local>
2022-09-27 08:59:53 +1300

    refactor: skills state

--- pages/skills.tsx
+++ pages/skills.tsx
@@ -1,49 +1,37 @@
 import { Grid, GridItem, Progress } from "@chakra-ui/react";
 import { NextPage } from "next";
+import { useEffect, useState } from "react";
 
 const Skills: NextPage = () => {
+  const [skills, setSkills] = useState([
+    { name: "Frontend", value: 80, colorScheme: "purple" },
+    { name: "Backend", value: 60, colorScheme: "cyan" },
+    { name: "ReactJS", value: 60, colorScheme: "green" },
+    { name: "Typescript", value: 50, colorScheme: "red" },
+    { name: "NextJS", value: 40, colorScheme: "yellow" },
+  ]);
+
+  useEffect(() => {
+    if (process.browser) {
+      // verify client/browser environment
+    }
+  });
+
   return (
     <Grid>
-      <GridItem>
-        <label>Frontend</label>
-        <Progress
-          colorScheme="cyan"
-          value={80}
-          hasStripe={true}
-          isAnimated={true}
-          size="xs"
-        />
-      </GridItem>
-      <GridItem>
-        <label>Backend</label>
-        <Progress
-          colorScheme="green"
-          value={60}
-          hasStripe={true}
-          isAnimated={true}
-          size="xs"
-        />
-      </GridItem>
-      <GridItem>
-        <label>CSS</label>
-        <Progress
-          colorScheme="red"
-          value={40}
-          hasStripe={true}
-          isAnimated={true}
-          size="xs"
-        />
-      </GridItem>
-      <GridItem>
-        <label>Animation</label>
-        <Progress
-          colorScheme="yellow"
-          value={30}
-          hasStripe={true}
-          isAnimated={true}
-          size="xs"
-        />
-      </GridItem>
+      {skills.map((skill) => (
+        <GridItem>
+          <label>{skill.name}</label>
+          <Progress
+            id="frontends"
+            colorScheme={skill.colorScheme}
+            value={skill.value}
+            hasStripe={true}
+            isAnimated={true}
+            size="xs"
+          />
+        </GridItem>
+      ))}
     </Grid>
   );
 };
