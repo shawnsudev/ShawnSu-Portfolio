0ec2918fdadf95e975430a5c1ce42d1e638604dc
Shawn DI <shawndi@192-168-1-142.tpgi.com.au>
2023-05-05 12:16:39 +0800

    chore: use dangerousSetInnerHT ML property to allow text formatti ng for <Card/> description

--- components/Card/Card.tsx
+++ components/Card/Card.tsx
@@ -63,7 +63,7 @@ const Card = ({ card, w }: CardProps) => {
           align="left"
         >
           <Box>
-            <Box>{card.description}</Box>
+            <Text dangerouslySetInnerHTML={{ __html: card.description }} />
             <Flex justify={"space-between"} mt="1rem">
               <Link href={card.link} isExternal>
                 <Text
